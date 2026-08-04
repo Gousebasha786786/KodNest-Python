0fd7b89acc91d46794c909c4e727d50a0b3fa766
jani-provab <shaik.jani@provabmail.com>
2026-08-04 17:33:58 +0530

    Read_and_Convert_Student_Details

---
+++ track/M02-intro of python/T02/M02-T02-ST02-L-intro-of-python/work/src/Read and Convert Student Details.py
@@ -0,0 +1,12 @@
+# Read and convert the student details
+Student_name = input()
+age= int (input())
+rating= float (input())
+# Display the values
+print (f"Student: {Student_name}")
+print (f"Age: {age}")
+print (f"Rating: {rating}")
+# Display the data types
+print (type(Student_name))
+print (type(age))
+print (type(rating))
