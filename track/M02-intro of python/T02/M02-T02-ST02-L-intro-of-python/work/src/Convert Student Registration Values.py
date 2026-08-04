f723c09fcc60a78e179249276df52354476f7295
jani-provab <shaik.jani@provabmail.com>
2026-08-04 17:21:01 +0530

    Convert_Student_Registration_Values

---
+++ track/M02-intro of python/T02/M02-T02-ST02-L-intro-of-python/work/src/Convert Student Registration Values.py
@@ -0,0 +1,15 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+# Convert the values here
+age = (int (age_text))
+fee = (float (course_fee_text))
+attempts=str(attempts)
+# Display the converted values and their data types
+print(age)
+print(type(age))
+print (fee)
+print(type(fee))
+print(attempts)
+print(type(attempts))
