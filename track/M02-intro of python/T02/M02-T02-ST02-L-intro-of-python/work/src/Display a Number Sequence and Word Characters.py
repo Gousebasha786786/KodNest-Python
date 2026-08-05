dcc3dc1cd4c37ab7cf8d1a8926bd6d22d71f8373
jani-provab <shaik.jani@provabmail.com>
2026-08-05 18:11:45 +0530

    Display_a_Number_Sequence_and_Word_Characters

---
+++ track/M02-intro of python/T02/M02-T02-ST02-L-intro-of-python/work/src/Display a Number Sequence and Word Characters.py
@@ -0,0 +1,12 @@
+# Read the number and word
+n=int(input())
+w=input()
+
+# Print the number sequence
+print ("Numbers:")
+for i in range(1,n+1):
+    print (i)
+# Print the characters
+print ("Characters:")
+for i in w:
+    print(i)
